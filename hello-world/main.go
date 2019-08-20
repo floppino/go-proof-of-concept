bc1e45f2bd3f2d77f9747b1fcbf942b5a56d801e
pc-ko2 <pc-ko2@MacBook-Pro-di-pc-ko2.local>
2019-08-20 17:06:20 +0200

    add comment

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,5 +1,3 @@
-// Playing areound with APIs
-
 package main
 
 import (
@@ -7,43 +5,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
+	// repo that let us differentiate APIs calls POST,GET etc
 	"github.com/gorilla/mux"
 )
 
-// Movie item
+// Movie item, creating a struct of type Movie with the following attributes
 type Movie struct {
 	Title   string `json:"title"`
 	Year    int    `json:"pub_year"`
 	Content string `json:"content"`
 }
 
-// Movies list
+// Movies list, creating a list that will contain the Movies
 type Movies []Movie
 
+// Lists all movies in the body of the function
 func allMovies(w http.ResponseWriter, r *http.Request) {
 	movies := Movies{
 		Movie{Title: "Alien", Year: 1979, Content: "Ellen Ripley muore"},
 	}
 
 	fmt.Println("All movies")
+	// Encode: convert data to and from byte-level and textual representations
 	json.NewEncoder(w).Encode(movies)
 }
 
+// Post call to insert our json via postman
 func postMovies(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Post movies work")
+	fmt.Fprintf(w, "Post call works... ?")
 }
 
+// Homepage for our root endpoint
 func homepage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "Hello Github!")
 }
 
 func handleRequests() {
-
+	// MyRouter: let us use different APIs on the same url (GET,POST etc)
 	MyRouter := mux.NewRouter().StrictSlash(true)
+	// Routes
 	MyRouter.HandleFunc("/", homepage)
 	MyRouter.HandleFunc("/movies", allMovies).Methods("GET")
 	MyRouter.HandleFunc("/movies", postMovies).Methods("POST")
+	// Open the server on that port, log.Fatal listens for errors and closes the connection if needed
 	log.Fatal(http.ListenAndServe(":8081", MyRouter))
 }
 
